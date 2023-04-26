7ae857a57d487139ad9005d86000abc63cb75b16
Alejandro <chimpi1370@gmail.com>
2023-04-26 17:11:13 +0000

    commit Configuracion de instalacion de paquetes con manejador de paquetes pnpm

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,4 @@
-export default function DashboardLayout({
-    children,
-  }: {
+export default function DashboardLayout({  children, }: {
     children: React.ReactNode,
   }) {
     return <section>{children}</section>;
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+interface DashboardCardProps {
+  title: string;
+  content: string;
+}
+const DashboardCard: React.FC<DashboardCardProps> = ({ title, content }) => {
+  return (
+    <div className="dashboard-card">
+      <div className="card-content">
+        <h2>{title}</h2>
+        <p>{content}</p>
+      </div>
+      <div className="card-actions">
+        <button className="button">Más información</button>
+      </div>
+    </div>
+  );
+};
 function dashboard() {
     return (
-    <div>dashboard</div>
+    <div className="dashboard-container">
+      <DashboardCard title="Card 1" content="Contenido de la tarjeta 1" />
+      <DashboardCard title="Card 2" content="Contenido de la tarjeta 2" />
+      <DashboardCard title="Card 3" content="Contenido de la tarjeta 3" />
+      <DashboardCard title="Card 4" content="Contenido de la tarjeta 4" />
+    </div>
     )
 }
 
